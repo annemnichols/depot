fa450c3320e598786a48a39c67a4a53a3fb109fa
annemnichols <anne@seantnichols.com>
2014-12-05 10:03:01 -0700

    Created basic shopping cart and button

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,12 @@
 class StoreController < ApplicationController
+  
   def index
   	@products = Product.order(:title)
+  	@counter_message = "You have accessed this page #{counter} times." if session[:counter] > 5
+  end
+
+  def counter
+  	session[:counter] ||= 0
+  	session[:counter] += 1
   end
 end
